b23a93225965b06edffdfe57bdcb5e4f1c97ae00
M.Goekche AYDIN <63980772+Gokay2705@users.noreply.github.com>
2023-09-02 17:19:51 +0200

    js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const navShow = () => {
-    
+    const menu=document.querySelector(".menu");
+    const nav=document.querySelector(".nav-links");
+
+    menu.addEventListener("click",()=>{
+        nav.classList.toggle("nav.active");
+    });
 }
+
+navShow();
