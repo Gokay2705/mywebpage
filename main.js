12d5db0b902e7e4288227d78d6831f1ec5517eaa
M.Goekche AYDIN <63980772+Gokay2705@users.noreply.github.com>
2023-09-03 00:11:22 +0200

    js

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 const navShow = () => {
-    const menu = document.querySelector(".menu");
-    const nav = document.querySelector(".nav-links");
-    const links = document.querySelectorAll(".nav-links li");
+    const menu = document.querySelector('.menu');
+    const nav = document.querySelector('.nav-links');
+    const links = document.querySelectorAll('.nav-links li');
 
-    menu.addEventListener('click', () => {
-        nav.classList.toggle("nav-active");
+    menu.addEventListener('click',()=>{
+        nav.classList.toggle('nav-active');
 
-        links.forEach((link, index)) => {
-    console.log(index);
-    if (link.style.animation) {
-        link.style.animation = '';
-    }
-    else {
-        link.style.animation = `linksFade .5s ease forwards ${index / 5 + 0.2}s`;
-    }
-});
-
+        links.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            }
+            else{
+                link.style.animation = `linksFade .5s ease forwards ${index / 5 + 0.2}s`;
+            }
+        });
+        menu.classList.toggle('close');
+    });
 };
 
 navShow();
